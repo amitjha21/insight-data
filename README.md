5912dc69ef9df872397836036509c27ae17248a8
amitjha21 <amitjha@usc.edu>
2018-04-04 03:28:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Insight Data Challenge
+
+
+### Created by [Amit Jha](mailto:amitjha@usc.edu)
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python ./src/sessionization.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
