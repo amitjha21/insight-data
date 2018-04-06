69b784f0fea3e06874098e2d11ebccdb0ca00294
amitjha21 <amitjha@usc.edu>
2018-04-06 00:53:34 -0700

    test cases added

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-## Insight Data Challenge
+## Insight Data Engineering Challenge
+### Steps to execute
+- Get the code from github
+- Go to project root directory and execute the following command from terminal:
+
+    ```
+    ./run.sh
+    ```
+- `run.sh` file contains following python command to execute the `sessionization.py`
+     ```
+     #!/usr/bin/env bash
+      python ./src/sessionization.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
+     ```
+
+- Output file `sessionization.txt` is created inside **output** folder
+- Test cases are maintained in `insight_testsuite` folder
+- There are three test cases: _my-test_1_, _my-test_2_, _test_1_
+- To execute test cases use following commands
+   ```
+    cd insight_testsuite
+    ./run_tests.sh
+   ```
+- The test output is maintained in `results.txt` inside _insight_testsuite_.
+   ```
+    [PASS]: my-test_1 sessionization.txt
+    [PASS]: my-test_2 sessionization.txt
+    [PASS]: test_1 sessionization.txt
+    3 of 3 tests passed
+   ```
+
+### Summary of program
+- All new IP request is maintained in _activeIpList_, _sortedIpList_ and _ipCollectDict_
+- If IP request already exists then update these 3 structures.
+- Session expiration is determined by the difference between current time and the last time an IP try to access any document.
+- Check the timestamp of each log entry. If a new timestamp is found, check if the session has expired in _sortedIpList_.
+- If the session is maintained for the longest seving IP then no need to check remaining IPs
+- IPs are removed after session has expired
+- Write all remaining entries at the end
 
 
-### Created by [Amit Jha](mailto:amitjha@usc.edu)
+
+#### Developed by [Amit Jha](mailto:amitjha@usc.edu)
--- src/sessionization.py
+++ src/sessionization.py
@@ -1,1 +1,89 @@
-def 
+import sys
+import datetime
+
+timestampFormat = "%Y-%m-%d %H:%M:%S"  #global constant
+
+def getInactivityValue(path):    # get inactivity_period
+    inactiveFile = open(path, 'r')
+    return int(inactiveFile.read())
+
+def getLogData(path):        # get initial log file
+    initialLogFile  = open(path, 'r')
+    logList = initialLogFile.readlines()
+    logList.pop(0)   # remove header from the data list
+    initialLogFile.close()
+    return logList
+
+def getTimeDiff(op1,op2):
+    return int((datetime.datetime.strptime(op2, timestampFormat) - datetime.datetime.strptime(op1, timestampFormat)).total_seconds())
+
+def writeOutputContent(path):    # write output to file
+    try:
+        outputWriter = open(path, 'w')
+    except:
+        print('Supply a valid path')
+        exit(1)
+    return outputWriter
+
+
+def getUserLogResults(logFilePath, inactivityPeriodFilePath, outputFilePath):
+
+    activeIpList = []
+    sortedIpList = []
+    ipCollectDict = dict()
+
+    inactivity_period = getInactivityValue(inactivityPeriodFilePath)
+    logs = getLogData(logFilePath)
+    writeToFile = writeOutputContent(outputFilePath)
+
+    for log in logs:
+        log = [x.strip() for x in log.split(',')]
+        ip = log[0]
+        initialStartTime = log[1]+' '+log[2]
+        
+        timestamp = datetime.datetime.strptime(initialStartTime, timestampFormat)
+        currentTime = initialStartTime
+
+        while sortedIpList:
+            userIP = sortedIpList[0]
+
+            if (timestamp - ipCollectDict[userIP]['sessionEndTime']).total_seconds() > inactivity_period:
+                values = ipCollectDict.pop(userIP)
+                sortedIpList.remove(userIP)
+                activeIpList.remove(userIP)
+
+                sessionStart = datetime.datetime.strftime(values['sessionStartTime'], timestampFormat)
+                sessionEnd = datetime.datetime.strftime(values['sessionEndTime'], timestampFormat)
+                writeToFile.write('%s,%s,%s,%d,%d\n' % (userIP, sessionStart, sessionEnd, getTimeDiff(sessionStart, sessionEnd) + 1,values['docsAccessed']))
+            else:
+                break
+
+        if ip not in activeIpList:
+            activeIpList.append(ip)
+            ipCollectDict[ip] = {'sessionStartTime': timestamp, 'sessionEndTime': timestamp, 'docsAccessed': 1}
+            sortedIpList.append(ip)
+
+        else:
+            ipCollectDict[ip]['sessionEndTime'] = timestamp
+            ipCollectDict[ip]['docsAccessed'] += 1    # update document accessed count by 1
+            sortedIpList.remove(ip)
+            sortedIpList.append(ip)
+
+    while len(activeIpList) > 0:  # write all remaining requests
+        userIP = activeIpList.pop(0)
+        values = ipCollectDict.pop(userIP)
+
+        sessionStart = datetime.datetime.strftime(values['sessionStartTime'], timestampFormat)
+        sessionEnd = datetime.datetime.strftime(values['sessionEndTime'], timestampFormat)
+        writeToFile.write('%s,%s,%s,%d,%d\n' % (userIP, sessionStart, sessionEnd, getTimeDiff(sessionStart, sessionEnd) + 1, values['docsAccessed']))
+        sortedIpList.remove(userIP)
+
+    writeToFile.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Pass all required parameters: " + str(len(sys.argv)))
+        exit(1)
+
+    getUserLogResults(sys.argv[1], sys.argv[2], sys.argv[3])
